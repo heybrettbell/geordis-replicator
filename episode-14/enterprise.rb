d3a6849805e9ca10d5780baf88fc63db60932c55
heybrettbell <brett@grassrootsonline.ca>
2017-07-05 22:38:21 -0400

    Episode 15 complete

--- episode-14/enterprise.rb
+++ episode-14/enterprise.rb
@@ -3,11 +3,11 @@ class Enterprise
   attr_reader :reactor, :transporter, :cupboard, :pantry, :replicator
 
   def initialize
-    @reactor     =     Reactor.new(self)
-    @transporter = Transporter.new(self)
-    @cupboard    =    Cupboard.new
-    @pantry      =      Pantry.new
-    @replicator  =  Replicator.new(self)
+    @reactor            =     Reactor.new(self)
+    @transporter        = Transporter.new(self)
+    @cupboard           =    Cupboard.new
+    @pantry             =      Pantry.new
+    @replicator         =  Replicator.new(self)
   end
 
 end
